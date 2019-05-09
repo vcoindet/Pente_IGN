4a3cc73d24e669e013a57c49b1d9ca8cfdd57c6b
hadriensi <hadrien.siney@me.com>
2019-05-09 17:49:10 +0200

    Ajout des options pour demo

--- Demo/js/bouton.js
+++ Demo/js/bouton.js
@@ -1,12 +1,27 @@
 var checkbox = document.getElementById('checke');
+var radio = document.getElementById('pointe');
 var elem_map = document.getElementById('mapid');
-//var LayerObject = L.layergoup();
+var effacer = document.getElementById('erase');
+var algor1 = document.getElementById('alg1');
+var algor2 = document.getElementById('alg2');
+var unite1 = document.getElementById('uni1');
+var unite2 = document.getElementById('uni2');
+var projection1 = document.getElementById('prj1');
+var projection2 = document.getElementById('prj2');
 
+
+
+var polyline = new L.Polyline([]).addTo(mymap);
 //console.log(elem_map.offsetLeft);
-//console.log(elem_map.offsetTop);
+//console.log(elem_map.offsetTop);    
 
-console.log(checkbox.checked);
-
+var theMarker ={};
+/**
+  * @function
+  * @name create_point
+  * @description Créé un marqueur à chaque click sur la carte et 
+	* renvoie sous forme de Popup la latitude et la longitude
+  */
 function create_point(){
 	mymap.on('click',function (e){
 		var lat = e.latlng.lat;
@@ -15,82 +30,117 @@ function create_point(){
 		let coord;
 		let donnee;
 		let pente;
-		
+		//Efface un marqueur lorsque celui existe déjà
 		if (theMarker != undefined) {     
 			mymap.removeLayer(theMarker);
 		}
 
-		//Add a marker to show where you clicked.
+		//Ajoute un marqueur à chaque click
 		theMarker = L.marker([lat,lon]).addTo(mymap);  
 		
 		coord = theMarker.getLatLng();
+
+		//Appelle le web service 
 		donnee = web_service(coord["lat"], coord["lng"]);
-		console.log(donnee);
 		//pente = donnee["pente"];
 		
 		theMarker.bindPopup('' + coord + "\n" + "pente = ").openPopup();
 	});
 }
-
-function web_service(lat, lng){
-	let data = "lat=" + lat + "&lng=" + lng + "&algo='Horn'&unit='deg'&proj=2154";
-	let ajax = new XMLHttpRequest();
-	ajax.open('GET','http://localhost:8080/point');
-	ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	ajax.responseType = "json";
-	ajax.send(data);
-	
-	
-	return ajax.response;
-}
-
+/**
+  * @function
+  * @name create_ligne
+  * @description Créé des lignes entre plusieurs marqueurs et renvoie
+	*  pour chaque marqueur sous forme de Popup la latitude et la longitude
+  */
 function create_ligne(){
 	mymap.on('click',function (e){
 		var lat = e.latlng.lat;
 		var lon = e.latlng.lng;
-		//var pour enregister les coords du marker
-		let coord;
-	
-		//Add a marker to show where you clicked.
+
+		if (theMarker != undefined) {     
+			mymap.removeLayer(theMarker);
+		}
+
+		//Ajoute un marqueur à chaque click
 		theMarker = L.marker([lat,lon]).addTo(mymap);  
 		theMarker.bindPopup('' + theMarker.getLatLng()).openPopup();
-		coord = theMarker.getLatLng();
-		//valeur de marker de type :
-		//  marker = {lat : 54, lng : 65}
-
-		//enregistre les coords dans le tableau
-		tab_point.push([coord["lat"], coord["lng"]]);
-
-		//console.log(tab_point);
-
-		if(tab_point.length > 2){
-			console.log('ligne');
-			
-			polyline = L.polyline([tab_point, {color: 'green'}]).addTo(mymap);
-		}
+		polyline.addLatLng(e.latlng);
 	});
 }
-
+/**
+  * @function
+  * @name chose_geom
+  * @description Choisit quelle fonction doit être utiliser 
+	* en fonction du bouton coché 
+  */
 function chose_geom(){
 	if(checkbox.checked){
 		create_ligne();
 	}
 
-	else{
+	if(radio.checked){
 		create_point();
 	}
 }
-
+/**
+  * @function
+  * @name clear
+  * @description A chaque click du bouton submit 'Supprimer élément de la 
+	* carte', supprime les lignes et les marqueurs présents sur la carte
+  */
 function clear (){
-	if (theMarker != undefined ) {     
-		
-		mymap.removeLayer(theMarker);
-		
-	}
-	if (polyline != undefined) {
-	  mymap.removeLayer(LayerObject);  
-	}
+	mymap.removeLayer(theMarker);
+	polyline.setLatLngs([]); 
 }
 
 elem_map.addEventListener("click", chose_geom);
-checkbox.addEventListener("click",clear);
+effacer.addEventListener("click",clear);
+
+function algo(){
+	let algs;
+	if (document.getElementById('alg1').checked) {
+		algs = document.getElementById('alg1').value;
+    }
+
+	else {
+		algs = document.getElementById('alg2').value;
+	}
+	return algs;
+}
+
+function uni(){
+	let united;
+	if (document.getElementById('uni1').checked) {
+		united = document.getElementById('uni1').value;
+	}
+ 
+	else{
+		united = document.getElementById('uni2').value;
+	}
+	return united
+}
+
+function project(){
+	let proj;
+	if (document.getElementById('prj1').checked) {
+		proj = document.getElementById('prj1').value;
+    }
+  
+ 	else{
+		proj = document.getElementById('prj2').value;
+	}
+	
+	return proj;
+}
+
+function web_service(lat, lng){
+	let data = "lat=" + lat + "&lng=" + lng + "&algo="+algo()+"&unit="+uni()+"&proj="+project();
+	let ajax = new XMLHttpRequest();
+	ajax.open('GET','http://localhost:8080/point');
+	ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	ajax.responseType = "json";
+	ajax.send(data);
+	return ajax.response;
+}		
+
--- Demo/js/carte.js
+++ Demo/js/carte.js
@@ -1,48 +1,58 @@
-var mymap = L.map('mapid').setView([48.8455901,2.4230108], 13);
+var mymap = L.map('mapid').setView([45.8898586,6.128757], 13);
 var osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
+	transparent:true
 }).addTo(mymap);
 
-// window.onload= function() {
-//     L.tileLayer(
-//             'https://wxs.ign.fr/keal590caiyuc4ei9ilrc0qd/geoportail/wms?service=WMS&request=GetTile&version=1.0.0&tilematrixset=PM&tilematrix={z}&tilecol={x}&tilerow={y}&layer=ORTHOIMAGERY.ORTHOPHOTOS&format=image/jpeg&style=normal',
-//             {
-//                 minZoom : 0,
-//                 maxZoom : 18,
-//                 tileSize : 256,
-//                 attribution : "IGN-F/Géoportail"
-//             }).addTo(mymap)
-//     }
-// // Permet de choisir quels couches de données à afficher
-// var baseMaps = {
-//     "Géoportail" : L.tileLayer(
-//             'https://wxs.ign.fr/keal590caiyuc4ei9ilrc0qd/geoportail/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=PM&tilematrix={z}&tilecol={x}&tilerow={y}&layer=ORTHOIMAGERY.ORTHOPHOTOS&format=image/jpeg&style=normal',
-//             {
-//                 minZoom : 0,
-//                 maxZoom : 18,
-//                 tileSize : 256,
-//                 attribution : "IGN-F/Géoportail"
-//             }).addTo(mymap)
-// }
-// //Bouton à cocher qui permet de cocher et décocher les cartes que l'on veut afficher ou non 
-// var overlayMaps = {
-//     "OpenStreetMap" : osm
-// }
+window.onload= function() {
+    L.tileLayer(
+            'https://wxs.ign.fr/pratique/geoportail/wms?service=WMS&request=GetTile&version=1.0.0&tilematrixset=PM&tilematrix={z}&tilecol={x}&tilerow={y}&layer=ORTHOIMAGERY.ORTHOPHOTOS&format=image/jpeg&style=normal',
+            {
+                minZoom : 0,
+                maxZoom : 18,
+                tileSize : 256,
+				attribution : "IGN-F/Géoportail",
+				transparent: true
+				
+            }).addTo(mymap)
+    }
+// Permet de choisir quels couches de données à afficher
+var baseMaps = {
+    "Géoportail" : L.tileLayer(
+            'https://wxs.ign.fr/pratique/geoportail/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=PM&tilematrix={z}&tilecol={x}&tilerow={y}&layer=ORTHOIMAGERY.ORTHOPHOTOS&format=image/jpeg&style=normal',
+            {
+                minZoom : 0,
+                maxZoom : 18,
+                tileSize : 256,
+				attribution : "IGN-F/Géoportail",
+				transparent: true
+            }).addTo(mymap)
+}
+//Bouton à cocher qui permet de cocher et décocher les cartes que l'on veut afficher ou non 
+var overlayMaps = {
+	"OpenStreetMap" : osm
+	
+}
 
-// L.control.layers(baseMaps,overlayMaps).addTo(mymap);
+L.control.layers(baseMaps,overlayMaps).addTo(mymap);
 var osmGeocoder = new L.Control.OSMGeocoder();
 mymap.addControl(osmGeocoder);
-
+L.control.scale().addTo(mymap);
 //layer pour les marker
 var theMarker = {};
+var mousePosition = L.geoportalControl.MousePosition({
+	displayCoordinate : false,
+	altitude : {
+			triggerDelay : 500
+	} 
+});
+mymap.addControl(mousePosition);
 
-//layer pour les lignes
-var theMarkerLigne = {};
 
-//init tableau de point pour la polyligne
-var tab_point = [];    
-var polyline = [];
+
+
+
 
 //  mymap.on('click',function Pointe(e){
 //   var lat = e.latlng.lat;
